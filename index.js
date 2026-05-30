2c91668660bd00b7d693dadc1a0ab2ad757ee16e
unknown <anaswarakambaran@gmail.com>
2026-05-30 19:58:54 +0530

    fixed cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = express()
 //enable cors in server
 server.use(cors({
     origin: ["http://localhost:5173",
-        "https://restaurant-pos-system-frontend-8u2c.vercel.app/auth"],
+        "https://maison-pos.vercel.app"],
     credentials: true
 }))
 //parse json to js content
